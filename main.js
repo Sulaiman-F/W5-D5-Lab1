b99586c1b31ce0d9f97e63441c4bcb5b8af43628
Sulaiman-F <s.alfawzan18@gmail.com>
2025-05-15 13:56:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,58 @@
+
+let root = document.getElementById("root");
+let element =
+
+    <div className="container">
+        <div className="info">
+            <h1>Sulaiman Alfawzan</h1>
+            <div className="contact">
+                <a className="link" href="">Email: sulaiman@example.com</a>
+                <a className="link" href="">Saudi Arabia</a>
+                <a className="link" href="tel:+1234567890">Phone: +1234567890</a>
+                <a className="link" href="https://www.linkedin.com/in/sulaimanalfawzan">LinkedIn</a>
+            </div>
+        </div>
+        <div className="profile">
+            <h4>Profile</h4>
+            <p>Recent Computer Science graduate with a strong analytical mindset and a passion for combining technology and business strategy. Successfully completed a 6-month Business Development trainee program, gaining hands-on experience in market research, client engagement, and strategic planning. Proficient in leveraging technical expertise to identify growth opportunities and implement impactful solutions. Highly adaptable, detail-oriented, and eager to contribute a unique blend of technical and business skills to a dynamic team.
+            </p>
+        </div>
+        <div className="profile">
+            <h4>Education</h4>
+            <div className="education">
+                <h5>Bachelor Degree in Computer Science</h5>
+                <p>2020 – 2025</p>
+            </div>
+            <div className="education">
+                <p>Qassim University</p>
+                <p>Qassim, Saudi Arabia</p>
+            </div>
+        </div>
+        <div className="profile">
+            <h4>Projects</h4>
+            <div className="project">
+                <h5>Link Up</h5>
+                <p>Link Up is a mobile application that connects university students within their college community, allowing them to stay informed about upcoming events, sign up for attendance, and communicate with other attendees. It aims to create a sense of community by providing a user-friendly experience for students to find events, connect with like-minded peers, and build relationships.</p>
+            </div>
+        </div>
+        <div className="profile">
+            <h4>WAEI</h4>
+            <div className="project">
+                <h5>Link Up</h5>
+                <p>This project introduces an innovative and eco-friendly digital platform aimed at enhancing the process of disposing, selling, and repairing used household items. aligned with global sustain ability goals, such as Saudi Vision 2030, the platform addresses significant challenges related to the disposal of used furniture and home appliances that traditionally contribute to pollution and resource depletion.</p>
+            </div>
+        </div>
+        <div className="Experience">
+            <div className="Experience-title">
+                <h4>Professional Experience</h4>
+                <p>Recent Computer Science graduate with a strong analytical mindset and a passion for combining technology and business strategy. Successfully completed a 6-month Business Development trainee program, gaining hands-on experience in market research, client engagement, and strategic planning. Proficient in leveraging technical expertise to identify growth opportunities and implement impactful solutions. Highly adaptable, detail-oriented, and eager to contribute a unique blend of technical and business skills to a dynamic team.
+                </p></div>
+            <div className="Experience-con"> <p>07/2024 – 01/2025</p>
+                <p>Riyadh, Saudi Arabia</p></div>
+        </div>
+    </div>
+
+
+
+    ;
+ReactDOM.render(element, root);
